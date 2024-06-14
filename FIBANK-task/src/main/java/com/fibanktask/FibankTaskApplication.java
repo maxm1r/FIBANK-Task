5464be4b97e61d1afff4e10b7aaa64e98193c583
Martin Ivanov <martololo16@gmail.com>
2024-06-14 12:09:14 +0300

    created the project

---
+++ FIBANK-task/src/main/java/com/fibanktask/FibankTaskApplication.java
@@ -0,0 +1,13 @@
+package com.fibanktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibankTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FibankTaskApplication.class, args);
+    }
+
+}
---
+++ FIBANK-task/src/test/java/com/fibanktask/FibankTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fibanktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibankTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
